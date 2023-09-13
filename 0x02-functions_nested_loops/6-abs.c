221498cfd2856ca814e4225846fd1883ee91381b
Mohamedtibshk <Mohamed7Ibrahim8@gmail.com>
2023-09-13 08:22:55 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,16 @@
 #include "main.h"
- /**
-  * _abs - function to find absolute value of a number
-  * @a: function parameter
-  * Return: a or -a;
-  */
+
+/**
+ *_abs - function to find absolute of a number
+ * @a: function parameter
+ * Return: a or -a;
+ */
 int _abs(int a)
 {
-	if (a > 0)
-	return (-a);
-	else if (a <= 0)
+	if (a < 0)
+		return (-a);
+	else if (a >= 0)
 	{
-	return (a);
+		return (a);
 	}
 }
