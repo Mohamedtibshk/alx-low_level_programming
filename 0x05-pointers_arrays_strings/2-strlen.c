b9bc30894c9c1566ff37479273737d28bfd6f0a9
Mohamedtibshk <Mohamed7Ibrahim8@gmail.com>
2023-09-18 14:27:15 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*_strlen - finds the length of a srring
+ * @s: string to be counted
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	return (count);
+}
